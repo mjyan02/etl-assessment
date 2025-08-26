55d0ee29a777b1f3b318629d6b271f2c571b0c8a
mjyan02 <mjyan02@gmail.com>
2025-08-26 16:59:18 -0700

    Update dependencies

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\ymj53\OneDrive\Documents\My Jobs\GovTech\ETL-Data-Modelling\venv"
+set "VIRTUAL_ENV=C:\Users\ymj53\OneDrive\Documents\My Jobs\GovTech\etl-assessment\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
